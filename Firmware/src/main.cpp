3abae8b74a8ae6d4d8e881cdb11220b1b7c9fdcb
Jose Tow <jose.towe@gmail.com>
2021-05-09 15:32:14 -0500

    Updated the code to arduino
    The Action queue is missing

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -10,16 +10,18 @@
  */
 
 #include <Arduino.h>
+#include <HID.h>
+#include <Keyboard.h>
 #include <Encoder.h>
 
 #include "configuration.h"
 
 #include "pins.h"
-#include "actions.h"
+// #include "actions.h"
 #include "buttons.h"
 #include "knobs.h"
 
-ActionQueue actionQueue;
+// ActionQueue actionQueue;
 Button buttonMatrix[16];
 Knob* knobs[4] = {
   new Knob(topKnobA, topKnobB),
@@ -33,6 +35,9 @@ void setup()
   #ifdef DEBUG
     Serial.begin(9600);
   #endif
+
+  // Starts the keyboard
+  Keyboard.begin();
 
   // Sets the keyboard matrix up
   for (int i = 0; i < 4; i++)
@@ -50,21 +55,26 @@ void loop()
 {
   // Reads the keyboard matrix and stores it in the buttons
   for (int i = 0; i < 4; i++) {
-    // Sets the row pin to Low to read each column
+     // Sets the row pin to Low to read each column
     digitalWrite(rowPins[i], LOW);
 
     // Reads each column
     for (int j = 0; j < 4; j++) {
+
+        bool buttonState = !digitalRead(colPins[j]);
+
+        int pos =  (j*4) + i;
+        buttonMatrix[pos].setState(buttonState);
+
         #if defined(DEBUG) && defined(DEBUG_DUMP_MATRIX)
+        if(buttonState) {
           Serial.print("Pressed key: ");
           Serial.print(j);
           Serial.print(",");
           Serial.println(i);
+        }
         #endif
-
-        int pos =  (j*4) + i;
-        buttonMatrix[pos].setState(!digitalRead(colPins[j]));
-
+        
     }
 
     // Sets the row pin back to HIGH
@@ -82,7 +92,7 @@ void loop()
         Serial.println(i);
       #endif
 
-      actionQueue.queueAction(buttonAssignments[i]);
+      // actionQueue.queueAction(buttonAssignments[i]);
     }
   }
 
@@ -106,23 +116,23 @@ void loop()
           Serial.println("CW");
         #endif
 
-        // Enqueues the respective action
-        switch (i) {
-          case 0:
-            actionQueue.queueAction(topKnobAssignments[0]);
-            break;
-          case 1:
-            actionQueue.queueAction(midKnobAssignments[0]);
-            break;
-          case 2:
-            actionQueue.queueAction(botKnobAssignments[0]);
-            break;
-          case 3:
-            actionQueue.queueAction(wheelAssignments[0]);
-            break;
-          default:
-            break;
-        }
+      //   // Enqueues the respective action
+      //   switch (i) {
+      //     case 0:
+      //       actionQueue.queueAction(topKnobAssignments[0]);
+      //       break;
+      //     case 1:
+      //       actionQueue.queueAction(midKnobAssignments[0]);
+      //       break;
+      //     case 2:
+      //       actionQueue.queueAction(botKnobAssignments[0]);
+      //       break;
+      //     case 3:
+      //       actionQueue.queueAction(wheelAssignments[0]);
+      //       break;
+      //     default:
+      //       break;
+      //   }
       }
 
       // CCW movement
@@ -132,28 +142,28 @@ void loop()
         #endif
 
         // Enqueues the respective action
-        switch (i) {
-          case 0:
-            actionQueue.queueAction(topKnobAssignments[1]);
-            break;
-          case 1:
-            actionQueue.queueAction(midKnobAssignments[1]);
-            break;
-          case 2:
-            actionQueue.queueAction(botKnobAssignments[1]);
-            break;
-          case 3:
-            actionQueue.queueAction(wheelAssignments[1]);
-            break;
-          default:
-            break;
-        }
+        // switch (i) {
+        //   case 0:
+        //     actionQueue.queueAction(topKnobAssignments[1]);
+        //     break;
+        //   case 1:
+        //     actionQueue.queueAction(midKnobAssignments[1]);
+        //     break;
+        //   case 2:
+        //     actionQueue.queueAction(botKnobAssignments[1]);
+        //     break;
+        //   case 3:
+        //     actionQueue.queueAction(wheelAssignments[1]);
+        //     break;
+        //   default:
+        //     break;
+        // }
       }
 
     }
 
   }
 
-  // Executes the queued actions
-  actionQueue.executeActions();
+  // // Executes the queued actions
+  // actionQueue.executeActions();
 }
